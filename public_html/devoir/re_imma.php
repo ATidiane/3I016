87158a56504a53e93d70042e2069745912593bea
System Administrator <baldeahmedtidiane36@gmail.com>
2016-10-09 08:09:57 +0200

    delete

--- public_html/devoir/re_imma.php
+++
@@ -1,4 +0,0 @@
-<?php
-error_reporting(E_ALL);
-define ('RE_IMMA', '/[A-Z]{2}-[0-9]{3}-[A-Z]{2}/');
-?>
--- public_html/saisie_fiable.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL);
-
-function saisie_fiable($tab, $index, $re='', $val='';) {
-  if (!isset($tab[$index])) return $val;
-  return (!$re or preg_match($re, $tab[$index])) ? true : false;
-}
-
-?>
